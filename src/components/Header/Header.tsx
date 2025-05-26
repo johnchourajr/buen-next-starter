379de05ce6fc343cd440a88236d043cefd94a1f0
johnchourajr <hi@john.design>
2025-05-25 23:50:34 -0700

    Remove unused Page component and related metadata generation logic from the project. Simplify Header component by eliminating dynamic navigation links, retaining only the Home link.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,10 @@
 import { LogoMark } from "@/svg";
-import { pages } from "@/template/PageTemplate/content";
 import Link from "next/link";
 import { Bounded } from "../Bounded";
 import { GridItem } from "../GridItem/GridItem";
 
 export function Header() {
-  const navigation = [
-    { label: "Home", href: "/" },
-    ...pages.map((page) => ({
-      label: page.title,
-      href: `/${page.uid}`,
-    })),
-  ];
+  const navigation = [{ label: "Home", href: "/" }];
 
   return (
     <Bounded as="header" className="py-4 z-50 fixed w-full top-0">
